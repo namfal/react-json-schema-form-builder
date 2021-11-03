1e661900987cd2f223c4f07505a383bccc718a43
Nami Falhan <nami@ubeeko.com>
2021-11-03 14:20:35 +0100

    import bootstrap into the library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import FormBuilder from './formBuilder/FormBuilder';
 import PredefinedGallery from './formBuilder/PredefinedGallery';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export { FormBuilder, PredefinedGallery };
