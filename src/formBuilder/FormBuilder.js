f3abeb169bb27a311497bf9064514aaf84b9868c
Nami Falhan <nami@ubeeko.com>
2021-11-04 15:12:58 +0100

    remove bootstrap import

--- src/formBuilder/FormBuilder.js
+++ src/formBuilder/FormBuilder.js
@@ -22,7 +22,6 @@ import {
 import DEFAULT_FORM_INPUTS from './defaults/defaultFormInputs';
 import type { Node } from 'react';
 import type { Mods } from './types';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const useStyles = createUseStyles({
   formBuilder: {
